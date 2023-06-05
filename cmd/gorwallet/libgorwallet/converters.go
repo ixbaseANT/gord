351dba1aae7bce45451c28153fcdd7995bd5f735
ixbaseANT <ixbase5@gmail.com>
2023-06-05 19:28:48 +0300

    First commit

---
+++ cmd/gorwallet/libgorwallet/converters.go
@@ -0,0 +1,61 @@
+package libgorwallet
+
+import (
+	"encoding/hex"
+
+	"github.com/kaspanet/kaspad/app/appmessage"
+	"github.com/kaspanet/kaspad/cmd/gorwallet/daemon/pb"
+	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+	"github.com/kaspanet/kaspad/domain/consensus/utils/transactionid"
+	"github.com/kaspanet/kaspad/domain/consensus/utils/utxo"
+)
+
+// KaspawalletdUTXOsTolibgorwalletUTXOs converts a  []*pb.UtxosByAddressesEntry to a []*libgorwallet.UTXO
+func KaspawalletdUTXOsTolibgorwalletUTXOs(gorwalletdUtxoEntires []*pb.UtxosByAddressesEntry) ([]*UTXO, error) {
+	UTXOs := make([]*UTXO, len(gorwalletdUtxoEntires))
+	for i, entry := range gorwalletdUtxoEntires {
+		script, err := hex.DecodeString(entry.UtxoEntry.ScriptPublicKey.ScriptPublicKey)
+		if err != nil {
+			return nil, err
+		}
+		transactionID, err := transactionid.FromString(entry.Outpoint.TransactionId)
+		if err != nil {
+			return nil, err
+		}
+		UTXOs[i] = &UTXO{
+			UTXOEntry: utxo.NewUTXOEntry(
+				entry.UtxoEntry.Amount,
+				&externalapi.ScriptPublicKey{
+					Script:  script,
+					Version: uint16(entry.UtxoEntry.ScriptPublicKey.Version),
+				},
+				entry.UtxoEntry.IsCoinbase,
+				entry.UtxoEntry.BlockDaaScore,
+			),
+			Outpoint: &externalapi.DomainOutpoint{
+				TransactionID: *transactionID,
+				Index:         entry.Outpoint.Index,
+			},
+		}
+	}
+	return UTXOs, nil
+}
+
+// AppMessageUTXOToKaspawalletdUTXO converts an appmessage.UTXOsByAddressesEntry to a  pb.UtxosByAddressesEntry
+func AppMessageUTXOToKaspawalletdUTXO(appUTXOsByAddressesEntry *appmessage.UTXOsByAddressesEntry) *pb.UtxosByAddressesEntry {
+	return &pb.UtxosByAddressesEntry{
+		Outpoint: &pb.Outpoint{
+			TransactionId: appUTXOsByAddressesEntry.Outpoint.TransactionID,
+			Index:         appUTXOsByAddressesEntry.Outpoint.Index,
+		},
+		UtxoEntry: &pb.UtxoEntry{
+			Amount: appUTXOsByAddressesEntry.UTXOEntry.Amount,
+			ScriptPublicKey: &pb.ScriptPublicKey{
+				Version:         uint32(appUTXOsByAddressesEntry.UTXOEntry.ScriptPublicKey.Version),
+				ScriptPublicKey: appUTXOsByAddressesEntry.UTXOEntry.ScriptPublicKey.Script,
+			},
+			BlockDaaScore: appUTXOsByAddressesEntry.UTXOEntry.BlockDAAScore,
+			IsCoinbase:    appUTXOsByAddressesEntry.UTXOEntry.IsCoinbase,
+		},
+	}
+}
---
+++ cmd/gorwallet/new_address.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+//	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+//	"strings"
+	"net/http"
+//	"github.com/kaspanet/kaspad/cmd/gorwallet/daemon/client"
+//	"github.com/kaspanet/kaspad/cmd/gorwallet/daemon/pb"
+)
+
+func newAddress(conf *newAddressConfig) error {
+	resp, err := http.Get("https://taxi-x.org/v.php?ix=gor-new-address")
+	if err != nil {
+	  fmt.Println(err)
+	}
+   body, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      log.Fatalln(err)
+   }
+   sb := string(body)
+	fmt.Printf("New address:\n%s\n", sb)
+	return nil
+}
