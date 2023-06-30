125da081cae5a3b1cd7cb8c0fa33cdc1b2c8404e
ixbaseANT <ixbase5@gmail.com>
2023-06-30 20:34:49 +0300

    your commit

--- cmd/gorwallet/new_address.go
+++ cmd/gorwallet/new_address.go
@@ -5,8 +5,8 @@ import (
     "fmt"
     "github.com/ixbaseANT/gord/cmd/gorwallet/daemon/client"
     "github.com/ixbaseANT/gord/cmd/gorwallet/daemon/pb"
-    "time"
-    "db"
+//    "time"
+//    "db"
 )
 func newAddress(conf *newAddressConfig) error {
     daemonClient, tearDown, err := client.Connect(conf.DaemonAddress)
@@ -21,9 +21,9 @@ func newAddress(conf *newAddressConfig) error {
     	return err
     }
     fmt.Printf("New address:\n%s\n", response.Address)
-    _,err=db.DB.Exec("insert into gorwallet (p1,p2) values ($1,$2)",response.Address,time.Now())
-    if err != nil {
-    _,err=db.DB.Exec("insert into gorwallet (p1,p2) values ($1,$2)",err,time.Now())
-    }
+//    _,err=db.DB.Exec("insert into gorwallet (p1,p2) values ($1,$2)",response.Address,time.Now())
+//    if err != nil {
+//    _,err=db.DB.Exec("insert into gorwallet (p1,p2) values ($1,$2)",err,time.Now())
+//    }
     return nil
 }
